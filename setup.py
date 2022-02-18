76fe2d7712f942728c0698434a6d86872fda67e5
Jordan Maxwell <nxtlabs13@gmail.com>
2022-02-17 20:28:37 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,3 @@
-"""
-Author: Jordan Maxwell
-Written: 08/04/2019
-
-The MIT License (MIT)
-
-Copyright (c) 2019 Nxt Games
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 try:
     from setuptools import setup
 except ImportError:
@@ -35,7 +8,7 @@ panda3d-rest provides a panda native solution to performing RESTful HTTP GET/POS
 """
 
 setup(
-    name='panda3d-rest',
+    name='panda3d_rest',
     description='panda3d-rest provides a panda native solution to performing RESTful HTTP GET/POST requests using non-blocking io.',
     long_description=long_description,
     license='MIT',
